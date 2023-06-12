43ff42a4b081869fe3f723af05f8650a85e36c58
Jeb <jebjosh@gmail.com>
2023-06-12 06:15:14 -0700

    Ng data tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      lengthMenu: [5, 6, 10, 25],
+      processing: true,
+    };
+  }
   title = 'TableData';
+
+  public dataa = [
+    {
+      name: 'Ajay',
+      email: 'therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    { name: 'Jas', email: 'therichpost@gmail.com', website: 'therichpost.com' },
+    {
+      name: 'therichpost',
+      email: 'therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    {
+      name: 'therichpost',
+      email: 'therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    { name: 'Jas', email: 'therichpost@gmail.com', website: 'therichpost.com' },
+    {
+      name: '8therichpost',
+      email: 'therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    {
+      name: '6therichpost',
+      email: '4therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    {
+      name: 'Jas',
+      email: 'utherichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    {
+      name: 'r`therichpost',
+      email: 'therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+    {
+      name: 'etherichpost',
+      email: 'therichpost@gmail.com',
+      website: 'therichpost.com',
+    },
+  ];
+
+  dtOptions: DataTables.Settings = {};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DataTablesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
